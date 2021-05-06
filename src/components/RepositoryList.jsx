f79b7ab12099a0504099240064eff480117c9ae3
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-06 10:42:59 -0300

    Listando repositorios do Github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,35 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
 
 export function RepositoryList() {
-  const repository = {
-    name: "teste",
-    description: "Forms in React",
-    link: "https://github.com/unform/unform",
-  };
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => {
+        const newRepositories = data.map((repository) => {
+          return {
+            id: repository.id,
+            name: repository.name,
+            description: repository.description,
+            link: repository.html_url,
+          };
+        });
+        setRepositories(newRepositories);
+      });
+  }, []);
 
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
+        {repositories.map((repository) => {
+          return <RepositoryItem key={repository.id} repository={repository} />;
+        })}
       </ul>
     </section>
   );
