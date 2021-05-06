957ff6d6272818274550fd6cf50eabd66bfaf9ef
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-06 08:13:08 -0300

    Estilizando listagem

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import "../styles/repositories.scss";
+
 export function RepositoryList() {
   const repository = {
     name: "teste",
@@ -8,7 +10,7 @@ export function RepositoryList() {
   };
 
   return (
-    <section>
+    <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
